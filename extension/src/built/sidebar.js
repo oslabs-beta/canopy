e9c69f1c31f0263f3718cdda55324c42e5f5564c
Vance M Mcgrady <68658084+VanceMcGrady@users.noreply.github.com>
2022-09-03 14:57:26 -0700

    devtool working in browser

--- extension/src/built/sidebar.js
+++ extension/src/built/sidebar.js
@@ -1,4 +1,4 @@
 var ourBanner = document.querySelector('.banner');
 if (ourBanner)
-    ourBanner.innerHTML = 'test'; // `${window.location}`;
+    ourBanner.innerHTML = 'test2'; // `${window.location}`;
 //# sourceMappingURL=sidebar.js.map
