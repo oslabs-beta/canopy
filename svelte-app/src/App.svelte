62a20ef1e4ef22b53e79ceb61306804f6e9922a4
Vance M Mcgrady <68658084+VanceMcGrady@users.noreply.github.com>
2022-09-06 10:34:48 -0700

    deleted svelte-app folder

--- svelte-app/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	let name = 'world';
-	// import { current_component } from "svelte/internal" 
-	// import {writable } from 'svelte/store'
-
-	// document.addEventListener('SveleteRegisterCompoent')
-</script>
-
-<h1>Hello {name}!</h1>
