f195905023065f08ebffc8841e2d7a1171317481
jhpaige <jhpaige@usc.edu>
2022-09-06 16:22:11 -0700

    Resorted extension so can build state tracker and component tracker with svelte and typescript

--- extension/src/sidebar.ts
+++
@@ -1,7 +0,0 @@
-import { current_component } from "svelte/internal" 
-// import {writable } from 'svelte/store'
-
-const ourBanner: Element | null = document.querySelector('.banner');
-
-if (ourBanner) ourBanner.innerHTML = current_component // this used to be: `${window.location}`;
-
