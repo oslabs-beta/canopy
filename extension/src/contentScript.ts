366306e5e0be094ba2ea3c6f763ab0b4eb6062a7
jhpaige <jhpaige@usc.edu>
2022-09-12 18:51:08 -0700

    Reformatted contentscript.ts to include more listeners

--- extension/src/contentScript.ts
+++ extension/src/contentScript.ts
@@ -21,13 +21,86 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
         body.children[0].remove();
       }
       // Sets text of injected page to necessary injection functions and original format
-      injectedPage.text = `
-      Front js
+      injectedPage.text = `(function () {
+          const deepCopy = (e) => JSON.parse(JSON.stringify(e));
+          let cacheState = [];
+          const components = [];
+          let lastIndex = 0;
 
-      ${req.script};
+          // Send component state messages to window
+          const sendMessages = (componentStates) => {
+            window.postMessage({ 
+              target: 'CANOPY',
+              body: { 
+                componentStates: componentStates, 
+                cacheLength: cacheState.length 
+              }
+            });
+          };
 
-      Back js
-      `
+          // Adds event listener for 'SvelteRegisterComponent' (only in dev mode)
+          window.document.addEventListener('SvelteRegisterComponent', (e) => {
+            components.push(e.detail.component);
+          })
+          // Sets timeout to save and dispatch state after svelte register component event listener added
+          setTimeout(saveAndDispatchState, 0);
+
+          // Returns true if cache is empty OR
+          // if the most recent cache state is different AND if that state differs from the previous index
+          function checkIfChanged(componentState, i) {
+            if (!cacheState.length ||
+              (JSON.stringify(cacheState[cacheState.length - 1][i][1]) !== JSON.stringify(componentState[1])
+              && JSON.stringify(cacheState[lastIndex][i][1]) !== JSON.stringify(componentState[1]))) {
+              return true;
+            } else return false;
+          }
+
+          // Saves each state and component name cache state and sends necessary messages to application.
+          function saveAndDispatchState() {
+            const currState = [];
+            components.forEach((component) => {
+              currState.push([component, component.$capture_state(), component.constructor.name]);
+            })
+            // only add to cache & send messages if any state has actually changed
+            if (currState.some(checkIfChanged)) {
+            // if cacheState is logner than the last index, we are back in time and should start a new branch
+              if (cacheState.length > lastIndex){
+                cacheState = cacheState.slice(0, lastIndex + 1)
+              }
+              sendMessages(parse(currState));
+              cacheState.push([...currState]);
+              lastIndex = cacheState.length - 1;
+            }
+          }
+
+          // Sets up event listeners for state data changes
+          function setupListeners(root) {
+            root.addEventListener('SvelteRegisterBlock', (e) => saveAndDispatchState());
+            root.addEventListener('SvelteDOMSetData', (e) => saveAndDispatchState());
+            root.addEventListener('SvelteDOMInsert', (e) => saveAndDispatchState());
+          };
+
+          // Calls on listener setup
+          setTimeout(() => setupListeners(window.document));
+
+          ${req.script};
+
+          window.addEventListener(
+            "message",
+            (messageEvent) => {
+              if (messageEvent.data.target === 'CANOPY' && messageEvent.data.body === 'TIME_TRAVEL') {
+                const i = messageEvent.data.ctxIndex;
+                lastIndex = i;
+                if (cacheState[i]) {
+                  cacheState[i].forEach((componentState) => {
+                    componentState[0].$inject_state(componentState[1])
+                  })
+                }
+              }
+            },
+            false
+          );
+        })();`
 
       // Appends new injected page to document
       document.children[0].append(injectedPage);
