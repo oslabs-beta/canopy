12bf4f758efcd550187c0ef3c88b97d681e76d84
jhpaige <jhpaige@usc.edu>
2022-09-12 18:52:00 -0700

    Rebundled

--- extension/src/built/background.js
+++ extension/src/built/background.js
@@ -1,2 +1,46 @@
-console.log('This is canopy');
+// Variable to store main port
+let mainPort;
+// Listens for messages from client and runs posts message to port if valid
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // Passes messages to port if request target is valid
+    if (request && request.target === 'CANOPY') {
+        // If port exists posts message to port
+        if (mainPort)
+            mainPort.postMessage({ body: request.body });
+    }
+});
+// Adds listener to port
+chrome.runtime.onConnect.addListener((connectedPort) => {
+    // Assigns connected port to mainPort
+    mainPort = connectedPort;
+    // Adds event listener for port messages
+    mainPort.onMessage.addListener((message) => {
+        if (message.target !== 'CANOPY')
+            return;
+        // Injects content script into page, adding necessary page listeners
+        if (message.body === 'runContentScript')
+            chrome.tabs.executeScript({ file: './contentScript.js' });
+        // If chrome port recieves message to update script script is updated
+        if (message.body === 'updateScript') {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    target: 'CANOPY',
+                    body: 'INITIALIZE_PAGE',
+                    script: message.script,
+                });
+            });
+        }
+        // If chrome port recieves message to update index it send message to webpage
+        if (message.body === 'updateExtensionIndex') {
+            // Queries the current window and sends a corresponding message with the time travel index
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    target: 'CANOPY',
+                    body: 'TIME_TRAVEL',
+                    currentIndex: message.currentIndex,
+                });
+            });
+        }
+    });
+});
 //# sourceMappingURL=background.js.map
