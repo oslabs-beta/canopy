8163aedb29a68d0c23d592b6c95340ed9b721374
jhpaige <jhpaige@usc.edu>
2022-09-16 16:21:38 -0700

    Added basic connect functionality from already existing panel app.js

--- extension/sidebar-bundle-generator/src/App.svelte
+++ extension/sidebar-bundle-generator/src/App.svelte
@@ -1,27 +1,69 @@
 <script lang="ts">
 	
+	import { onMount } from 'svelte';
+	import ComponentTree from './ComponentTree.svelte';
+
+	export let snapshots = [];
+	
+	let currIndex = 0;
+	let chromePort;
+	$: currState = snapshots[currIndex];
+
+	// Connects chrome port to extension
+	const connectToPort = () => {
+		// Saves port to variable
+		chromePort = chrome.runtime.connect();
+		// Adds listener to chrome port to update component if corresponding message recieved
+		chromePort.onMessage.addListener((msg, sender, sendResponse) => {
+
+			// if (msg.target !== 'CANOPY') return;
+			if (!snapshots.includes(msg.body)) {
+				const moment = [];
+        msg.body.componentStates.forEach((state) => {
+          const obj = {};
+          obj[state[2]] = state[1];
+          moment.push(obj);
+        });
+				
+        snapshots = [...snapshots.slice(0, msg.body.cacheLength), moment];
+			}
+		});
+	}
+
+	// Injects script to page
+	const injectScript = async () => {
+		await chromePort.postMessage({
+			target: 'CANOPY',
+      body: 'runContentScript',
+    });
+		chrome.devtools.inspectedWindow.getResources((resources) => {
+      // search for bundle file, make sure its named bundle.js
+      for (let i = 0; i < resources.length; i++) {
+        if (resources[i].url.endsWith('bundle.js')) {
+          resources[i].getContent((content, encoding) => {
+            chromePort.postMessage({
+							target: 'CANOPY',
+              body: 'updateScript',
+              script: content,
+            });
+          });
+        }
+      }
+    });
+	}
+
+	// On component mount, connects page to port and injects script to page with updateScript command
+	onMount(async () => {
+		connectToPort();
+		await injectScript();
+	});
+
 </script>
 
 <main>
-	<h1>Component Data</h1>
-    <div class="tree-container">
-        <ul>
-            <li>Counter Component</li>
-            <li>component</li>
-			<li>[props: ] <br> [state: ]</li>
-			<ul>
-				<li>btn</li>
-				<li>component</li>
-				<li>[props:  ] <br> [data: ]</li>
-			</ul>
-			<ul>
-				<li>input</li>
-				<li>component</li>
-				<li>[props:  ] <br> [data: ]</li>
-			</ul>
-        </ul>
-		
-    </div>
+	<h1>Component Tree</h1>
+		<ComponentTree {currState} />
+   
 </main>
 
 <style>
