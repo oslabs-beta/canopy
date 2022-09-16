97f2a887097e7290bc73bf825a86b30fc0c50ee2
jhpaige <jhpaige@usc.edu>
2022-09-16 16:21:58 -0700

    Added component tree svelte application

---
+++ extension/sidebar-bundle-generator/src/ComponentTree.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+  export let currState;
+
+</script>
+
+<div>
+  <div class="tree-container">
+    <ul>
+        <li>Counter Component</li>
+        <li>component</li>
+  <li>[props: ] <br> [state: ]</li>
+  <ul>
+    <li>btn</li>
+    <li>component</li>
+    <li>[props:  ] <br> [data: ]</li>
+  </ul>
+  <ul>
+    <li>input</li>
+    <li>component</li>
+    <li>[props:  ] <br> [data: ]</li>
+  </ul>
+    </ul>
+</div>
+</div>
+
+<style>
+
+</style>
