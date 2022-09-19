6a02613d9753efec92719c4dca0472ebeb0369e7
jhpaige <jhpaige@usc.edu>
2022-09-19 10:05:16 -0700

    Prepped component tree for new logic

--- extension/sidebar-bundle-generator/src/ComponentTree.svelte
+++ extension/sidebar-bundle-generator/src/ComponentTree.svelte
@@ -1,46 +1,17 @@
 <script lang="ts">
 
-  export let currState;
+  export let currMoment;
 
 </script>
 
 <div>
   <div class="tree-container">
-    <ul>
-      <li>Counter Component</li>
-      <li>component</li>
-    <li>[props: ] <br> [state: ]</li>
-    <ul>
-      <li>btn</li>
-      <li>component</li>
-      <li>[props:  ] <br> [data: ]</li>
-    </ul>
-    <ul>
-      <li>input</li>
-      <li>component</li>
-      <li>[props:  ] <br> [data: ]</li>
-    </ul>
-      </ul>
+    {console.log(currMoment)}
   </div>
 </div>
 
 <style>
   .tree-container {
     background-color: rgb(196, 191, 191);
-  }
-  ul{
-    padding: 12px;
-    /* padding-inline-start: 0; */
-    list-style-type: none;
-  }
-
-  li{
-    list-style: none;
-  }
-  li::before{
-    content: "\00BB";
-  }
-  li :hover{
-    font-weight: bold;
   }
 </style>
