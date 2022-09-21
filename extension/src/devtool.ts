b6e50b33621a92206f5bf3ad5852efae7a6264d4
jhpaige <jhpaige@usc.edu>
2022-09-21 12:37:02 -0700

    Renamed sidebar pane

--- extension/src/devtool.ts
+++ extension/src/devtool.ts
@@ -6,7 +6,7 @@ chrome.devtools.panels.create("Canopy",
 );
 
 
-chrome.devtools.panels.elements.createSidebarPane("Svelte Component",
+chrome.devtools.panels.elements.createSidebarPane("Svelte Components",
   function(sidebar) {
     // sidebar initialization code here
     sidebar.setPage("/sidebar.html");
