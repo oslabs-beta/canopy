8543a4a3ec5381fcfdc6150cc22445613a4b8d0b
jhpaige <jhpaige@usc.edu>
2022-09-21 17:52:38 -0700

    Removed generated files from github

--- extension/src/built/devtool.js
+++
@@ -1,7 +0,0 @@
-//creates our Canopy tab in the devtools window and displays devtool.html
-chrome.devtools.panels.create("Canopy", "get_started32.png", "panel.html", function (panel) { });
-chrome.devtools.panels.elements.createSidebarPane("Svelte Components", function (sidebar) {
-    // sidebar initialization code here
-    sidebar.setPage("/sidebar.html");
-});
-//# sourceMappingURL=devtool.js.map
