f6706d1b3033c17ece626ba42df81f223f630c69
jhpaige <jhpaige@usc.edu>
2023-02-20 11:06:17 -0800

    Centered/cleaned up necessary content

--- demo-client/src/Slider.svelte
+++ demo-client/src/Slider.svelte
@@ -21,10 +21,13 @@
 	label { display: flex }
 	input, p { margin: 6px }
 
-    #slider-container {
-        margin: 30px;
-        border: 1px solid black;
-        padding: 1rem;
-        }
+	#slider-container {
+		margin: 30px;
+		border: 1px solid black;
+		padding: 1rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 </style>
 
